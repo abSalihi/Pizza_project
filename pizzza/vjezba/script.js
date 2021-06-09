abe9227f33168fd3b8158073d36f66900e9a9dbc
as3254 <as3254@g.rit.edu>
2021-06-09 15:07:22 +0200

    sri

--- pizzza/vjezba/script.js
+++ pizzza/vjezba/script.js
@@ -1,6 +1,17 @@
-function addinput() {
-    var name = document.getElementById("name").value;
-    console.log(name)
- 
-    document.getElementById("message").innerHTML = "output: " + name;
-}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    let addToDoButton = document.getElementById('add');
+    let list = document.getElementById('list');
+    let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    list.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('dblclick', function(){
+        list.removeChild(paragraph);
+    })
+});
+});
